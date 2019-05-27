32b52b935863eb434e698a5aa15169359182abee
Stargat3 <44499481+Stargat3@users.noreply.github.com>
2019-05-27 10:04:58 +0200

    controller

--- controller/src/main/java/controller/IBoulderDashController.java
+++ controller/src/main/java/controller/IBoulderDashController.java
@@ -1,5 +1,13 @@
 package controller;
 
 public interface IBoulderDashController {
+	
+	public void play();
+	
+	public IOrderPerformer getOrderPerformer();
+	
+	public void controller ();
+	
+	public void orderPerform (ControllerOrder controllerOrder);
 
 }
