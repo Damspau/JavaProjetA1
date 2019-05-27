4f3b82130314abe14b1fb31224a3c6344152209f
Damspau <44500326+Damspau@users.noreply.github.com>
2019-05-27 09:27:36 +0200

    Update

---
+++ contract/src/main/java/contract/IMap.java
@@ -0,0 +1,19 @@
+package contract;
+
+public interface IMap {
+	//get the width of the map
+	public void getWidth();
+	
+	//get the height of the map
+	public void getHeight();
+	
+	//go on the map
+	public IElements getOnTheMap (int x, int y);
+	
+	//notify the observer
+	public void setMobileHasChanged ();
+	
+	//become an observable
+	public Observable getObservable ();
+	
+}
