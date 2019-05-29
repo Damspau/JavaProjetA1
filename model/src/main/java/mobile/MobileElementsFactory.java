e7dd5b16fb00e7cc49a443d9288e96faf0ec3a66
Maxime-PC <Maxime-PC@Maxime-PC>
2019-05-29 20:36:17 +0200

    good setup

--- model/src/main/java/mobile/MobileElementsFactory.java
+++ model/src/main/java/mobile/MobileElementsFactory.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class MobileElementsFactory {
 	
-	private Player player = new Player ();
-	
-	public CommonMobile getFromFileSymbol (final char fileSymbol) throws IOException {
-		return player;
+//	private Player player = new Player ();
+//	
+//	public CommonMobile getFromFileSymbol (final char fileSymbol) throws IOException {
+//		return player;
 	}
 	
 
-}
+
