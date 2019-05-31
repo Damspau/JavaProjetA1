dab2c5eca12e14ec632d1a1f3791a1bbc47ce981
Maxime-PC <Maxime-PC@Maxime-PC>
2019-05-31 19:54:18 +0200

    maj

--- model/src/main/java/elements/Map.java
+++ model/src/main/java/elements/Map.java
@@ -9,124 +9,84 @@ import java.util.Observable;
 
 import org.showboard.ISquare;
 
-
-
 import contract.IElements;
 import contract.IMap;
+import entity.DB;
 import mobile.CommonMobile;
 import mobile.MobileElementsFactory;
-import mobile.Player;
 import motionless.CommonMotionless;
 import motionless.MotionlessElementsFactory;
 
 public class Map extends Observable implements IMap {
-	
+
 	private int width;
 	private int height;
-	private IElements [][] onTheMap;
-	private int actualXPlayer;
-	private int actualYPlayer;
-	private CommonMobile playerDetector;
-	
-	
-	public Map(final String fileName) throws IOException {
+	private IElements[][] onTheMap;
+
+	public Map(String fileName) throws IOException {
 		super();
+		DB.lireEnBase();
 		this.loadFile(fileName);
 	}
-	public static int compterOccurrencesLargeur(String line, char recherche)
-	{
-	 int nb = 0;
-	 for (int i=0; i < line.length(); i++)
-	 {
-	 if (line.charAt(i) == recherche)
-	 nb++;
-	 }
-	 return nb;
-	}
-	public static int compterOccurencesHauteur(String fileName) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(fileName));
-		int lines = 0;
-		while (reader.readLine() != null) lines++;
-		reader.close();
-		return lines;
-	}
 
-	//temporal reading for the map
 	private void loadFile(final String fileName) throws IOException {
-        final BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-        String line = buffer.readLine() ;
-        int y = 0;
-      
-        
-   
-        this.setWidth(compterOccurrencesLargeur(line, 'b'));
-       
-        this.setHeight(compterOccurencesHauteur(fileName));
-        
-        
-        
-        this.onTheMap = new IElements[this.getWidth()][this.getHeight()];
 
-    	MotionlessElementsFactory factoryMotionless = new MotionlessElementsFactory();
-    	MobileElementsFactory factoryMobile = new MobileElementsFactory();
-  
-    	
-    	
-    	
-    	
-    	
-    	//Elements display:
-    	
-        while (line != null) {
-        	
-            for (int x = 0; x < line.toCharArray().length; x++) {
-            	CommonMotionless motionLessElement = factoryMotionless.getFromFileSymbol(line.toCharArray()[x]);
-            	if (motionLessElement==null) {
-            		CommonMobile mobileElement = factoryMobile.getFromFileSymbol(line.toCharArray()[x],x,y);
-            		if (line.toCharArray()[x]=='p') {
-            			setActualXPlayer(x);
-            			setActualYPlayer(y);
-            			
-            		}
-            		mobileElement.setX(x);
-                	mobileElement.setY(y);
-                	
-                	this.setOnTheMap(mobileElement, x, y);
-            	}
-            	else {
-            		motionLessElement.setX(x);
-                	motionLessElement.setY(y);
-                    this.setOnTheMap(motionLessElement, x, y);
-            		
-            	}
-            	
-            }
-            line = buffer.readLine();
-            y++;
-        }
-        buffer.close();
-    }
-	
-	private void setOnTheMap(Elements mobileElement, int x, int y) {
-		this.onTheMap[x][y] = mobileElement;
-		
+		final BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		String line = buffer.readLine();
+		int y = 0;
+		this.setWidth(compterOccurrencesLargeur(line));
+		this.setHeight(compterOccurencesHauteur(fileName));
+
+		this.onTheMap = new IElements[this.getWidth()][this.getHeight()];
+
+		MotionlessElementsFactory factoryMotionless = new MotionlessElementsFactory();
+		MobileElementsFactory factoryMobile = new MobileElementsFactory();
+
+		// Elements display:
+
+		while (line != null) {
+
+			for (int x = 0; x < line.toCharArray().length; x++) {
+				System.out.println(line);
+				CommonMotionless motionLessElement = factoryMotionless.getFromFileSymbol(line.toCharArray()[x]);
+				if (motionLessElement == null) {
+					CommonMobile mobileElement = factoryMobile.getFromFileSymbol(line.toCharArray()[x]);
+					mobileElement.setX(x);
+					mobileElement.setY(y);
+					this.setOnTheMap(mobileElement, x, y);
+				} else {
+					motionLessElement.setX(x);
+					motionLessElement.setY(y);
+					this.setOnTheMap(motionLessElement, x, y);
+
+				}
+
+			}
+			line = buffer.readLine();
+			y++;
+		}
+		buffer.close();
 	}
-	
+
+	private void setOnTheMap(IElements mobileElement, int x, int y) {
+		this.onTheMap[x][y] = mobileElement;
+
+	}
+
 	@Override
 	public ISquare getOnTheMap(int x, int y) {
 		// TODO Auto-generated method stub
 		return (ISquare) this.onTheMap[x][y];
 	}
 
-
 	public void setWidth(int width) {
 		this.width = width;
-		
+
 	}
 
 	public void setHeight(int height) {
 		this.height = height;
-		
+
 	}
 
 	@Override
@@ -141,13 +101,11 @@ public class Map extends Observable implements IMap {
 		return this.height;
 	}
 
-
-
 	@Override
 	public void setMobileHasChanged() {
 		this.setChanged();
 		this.notifyObservers();
-		
+
 	}
 
 	@Override
@@ -155,23 +113,40 @@ public class Map extends Observable implements IMap {
 		// TODO Auto-generated method stub
 		return this;
 	}
+
+	public static int compterOccurrencesLargeur(String line) {
+		int nb = 0;
+		for (int i = 0; i < line.length(); i++) {
+			nb++;
+		}
+		return nb;
+	}
+
+	public static int compterOccurencesHauteur(String fileName) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		int lines = 0;
+		while (reader.readLine() != null)
+			lines++;
+		reader.close();
+		return lines;
+	}
+
+	@Override
 	public int getActualXPlayer() {
-		return actualXPlayer;
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	public void setActualXPlayer(int actualXPlayer) {
-		this.actualXPlayer = actualXPlayer;
-	}
+
+	@Override
 	public int getActualYPlayer() {
-		return actualYPlayer;
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	public void setActualYPlayer(int actualYPlayer) {
-		this.actualYPlayer = actualYPlayer;
-	}
+
 	@Override
 	public void setActualYPlayer() {
 		// TODO Auto-generated method stub
 		
 	}
-
 
 }
