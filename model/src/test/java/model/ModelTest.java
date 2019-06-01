8e0dcc69b7590c751d3b495cdef310cfa7cb9894
BenoitCesi <BenoitCesi@users.noreply.github.com>
2019-06-01 17:10:20 +0200

    Test making.

--- model/src/test/java/model/ModelTest.java
+++ model/src/test/java/model/ModelTest.java
@@ -1,9 +1,48 @@
-/**
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
 package model;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import elements.Map;
+
 public class ModelTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	private String filename = "main/map.txt";
+	private Map map;
+	private void setMap(Map map) {
+		this.map = map;
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBoulderdashModel() throws IOException {
+		this.setMap(new Map(filename));
+		assertNotNull("Map is not null", map);
+	}
+
+	
 
 }
